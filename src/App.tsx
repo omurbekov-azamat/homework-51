3eae750913c2794b76a35ea657653471ae3e5218
omurbekov-azamat <avgusttin8@gmail.com>
2022-10-19 18:47:51 +0600

    commit-5 created a useState and button for change numbers by using setNumbers

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Circle from "./Circle/Circle";
 
 function App() {
+  const [numbers, setNumbers] = useState([
+    {number: Math.floor(Math.random() * 32) + 5},
+    {number: Math.floor(Math.random() * 32) + 5},
+    {number: Math.floor(Math.random() * 32) + 5},
+  ]);
+
+  const changeNumber = () => {
+    setNumbers([
+      {number: Math.floor(Math.random() * 32) + 5},
+      {number: Math.floor(Math.random() * 32) + 5},
+      {number: Math.floor(Math.random() * 32) + 5},
+    ]);
+  };
+
   return (
     <div className="App">
-      <Circle number={Math.floor(Math.random() * 32) + 5}/>
-      <Circle number={Math.floor(Math.random() * 32) + 5}/>
-      <Circle number={Math.floor(Math.random() * 32) + 5}/>
+      <Circle number={numbers[0].number}/>
+      <Circle number={numbers[1].number}/>
+      <Circle number={numbers[2].number}/>
+      <button onClick={changeNumber}>start</button>
     </div>
   );
 }
