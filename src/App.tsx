f9f92af823b632f48567b7553b0130b5ae43a71a
omurbekov-azamat <avgusttin8@gmail.com>
2022-10-21 14:20:34 +0600

    commit-6 removed <React.StrictMode> in index.tsx create function for getting random number

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,37 @@ import Circle from "./Circle/Circle";
 
 function App() {
   const [numbers, setNumbers] = useState([
-    {number: Math.floor(Math.random() * 32) + 5},
-    {number: Math.floor(Math.random() * 32) + 5},
-    {number: Math.floor(Math.random() * 32) + 5},
+    0, 0, 0, 0, 0
   ]);
 
   const changeNumber = () => {
-    setNumbers([
-      {number: Math.floor(Math.random() * 32) + 5},
-      {number: Math.floor(Math.random() * 32) + 5},
-      {number: Math.floor(Math.random() * 32) + 5},
-    ]);
+
+    const arr:number[] = [];
+    const randomNumber = () => {
+      do {
+        let number = Math.floor(Math.random() * (36 - 5 + 1)) + 5;
+        if (arr.length === 0) {
+          arr.push(number);
+        }
+        if (!arr.includes(number)){
+          arr.push(number)
+        }
+        arr.sort(function(a, b){return a - b});
+      } while (arr.length < 5)
+      return arr
+     };
+
+    setNumbers(randomNumber);
   };
 
   return (
     <div className="App">
-      <Circle number={numbers[0].number}/>
-      <Circle number={numbers[1].number}/>
-      <Circle number={numbers[2].number}/>
-      <button onClick={changeNumber}>start</button>
+      <Circle number={numbers[0]}/>
+      <Circle number={numbers[1]}/>
+      <Circle number={numbers[2]}/>
+      <Circle number={numbers[3]}/>
+      <Circle number={numbers[4]}/>
+      <button onClick={changeNumber}>New numbers</button>
     </div>
   );
 }
